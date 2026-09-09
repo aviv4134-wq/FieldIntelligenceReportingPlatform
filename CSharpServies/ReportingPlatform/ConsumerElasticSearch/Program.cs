0e546620bc9eba5af99ff4e020ffbcf3f4aff853
avraham <aviv4134@gmail.com>
2026-09-09 14:44:29 +0300

    send data to elastic server after validation

--- CSharpServies/ReportingPlatform/ConsumerElasticSearch/Program.cs
+++ CSharpServies/ReportingPlatform/ConsumerElasticSearch/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Confluent.Kafka;
 using ConsumerElasticSearch.Models;
 using ConsumerElasticSearch.Servies;
+using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.Nodes;
+using Elastic.Transport;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -12,7 +16,7 @@ namespace ConsumerElasticSearch
 {
     class Program
     {
-        static void Main()
+        static async Task Main()
         {
             IConfiguration builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -23,8 +27,52 @@ namespace ConsumerElasticSearch
             string topic = builder["kafka:Topic:rawdata"]!;
             string groupId = builder["kafka:GroupId"]!;
 
+            var settings = new ElasticsearchClientSettings(new Uri("http://localhost:9200"))
+                .DefaultMappingFor<Report>(m => m.IdProperty(r => r.reportId));
+
+            var client = new ElasticsearchClient(settings);
+
+
             var service = new ServiceCollection();
 
+            service.AddSingleton(client);
+
+            var serviesProvidor = service.BuildServiceProvider();
+
+            using ( var scope = serviesProvidor.CreateScope())
+            {
+                var elasticClient = scope.ServiceProvider.GetRequiredService<ElasticsearchClient>();
+
+                var response  = await elasticClient.Indices.CreateAsync("reports", c => c
+                
+                .Mappings(mappings => mappings
+               
+                .Properties<Report>(p => p
+                .Keyword(r => r.reportId)
+                .Date(r => r.timestamp)
+                .Keyword(r => r.agentId)
+                .Keyword(r => r.unit)
+                .Text(r => r.theater)
+                .Keyword(r => r.sector)
+                .Keyword(r => r.location)
+                .Keyword(r => r.reportType)
+                .Keyword(r => r.priority)
+                .Keyword(r => r.sourceType)
+                .Text(r => r.message)
+                .Keyword(r => r.subjectId)
+                .Keyword(r => r.subjectType)
+                .Date(r => r.processedAt)      
+                )));
+
+                if (!response.IsValidResponse)
+                    Console.WriteLine("error index not created ");
+
+                else Console.WriteLine("index created");
+                
+            }
+
+            
+            
             var config = new ConsumerConfig
             {
                 BootstrapServers = bootstrap,
@@ -34,9 +82,6 @@ namespace ConsumerElasticSearch
             };
 
             
-
-
-
             var consumer = new ConsumerBuilder<string, string>(config).Build();
 
             consumer.Subscribe(topic);
@@ -47,7 +92,7 @@ namespace ConsumerElasticSearch
                 if (result == null || result.Message?.Value == null)
                     continue;
                
-                var report = result.Message.Value;
+                var report = JsonSerializer.Deserialize<Report>(result.Message.Value);
 
 
                 bool isValidReport = ValidatorReports.Validate(report);
@@ -56,7 +101,20 @@ namespace ConsumerElasticSearch
                 if (!isValidReport) 
                     continue;
 
-                Console.WriteLine(report);
+               var response = await client.PingAsync();
+                
+               if  (! response.IsValidResponse)
+                    Console.WriteLine("the server offline");
+
+                var IsExsists = await client.ExistsAsync("reports", report.reportId);
+
+                if (! IsExsists.Exists)
+                {
+                    var IsRportCreated = await client.IndexAsync(report, x => x.Index("reports"));
+                    if (! IsRportCreated.IsValidResponse)
+                        Console.WriteLine("report not created ");
+                    else Console.WriteLine("report created");
+                }
 
             }
         }
