f90230e78f0eed6949361af8e71cbbd81f4e4142
avraham <aviv4134@gmail.com>
2026-09-09 14:49:13 +0300

    update main json dump insted to str

--- PythonServies/main.py
+++ PythonServies/main.py
@@ -4,14 +4,15 @@ from ProducerServies import KafkaProducer
 
 
 def main():
+    coun = 0
     with open("Data/field_reports.json","r",encoding='UTF-8') as file_data:
         reports = json.load(file_data)
         for report in reports:
-           report = str( report)
+           report = json.dumps(report)
            KafkaProducer.sent_to_kafka(report)
-
+           coun+=1
         KafkaProducer.producer.flush()
-            
+        print(coun)    
 
 
 main()
