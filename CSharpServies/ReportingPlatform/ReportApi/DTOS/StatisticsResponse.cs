a5bd3f302dc85ed6f0b875b5f1c4d859ef0038f6
avraham <aviv4134@gmail.com>
2026-09-10 17:17:17 +0300

    add dto statistics

---
+++ CSharpServies/ReportingPlatform/ReportApi/DTOS/StatisticsResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace ReportApi.DTOS
+{
+    public class StatisticsResponse
+    {
+        
+        public Dictionary<string, long> ByPriority { get; set; } = new();
+
+        public Dictionary<string, long> ByReportType { get; set; } = new();
+
+        public Dictionary<string, long> ByTheater { get; set; } = new();
+
+    }
+}
