93ab7520c28f14339f2a2a8a95fcde55b5aa7a2b
avraham <aviv4134@gmail.com>
2026-09-10 17:16:16 +0300

    add irepository and repository for report quaries

---
+++ CSharpServies/ReportingPlatform/ReportApi/Repositories/IRepositoryReports.cs
@@ -0,0 +1,21 @@
+﻿using ConsumerElasticSearch.Models;
+using ReportApi.DTOS;
+
+namespace ReportApi.Repositories
+{
+    public interface IRepositoryReports
+    {
+        public Task<IEnumerable<Report>> GetByTextAsync(string text);
+
+        public Task<IEnumerable<Report>> GetBySunjectIdAsync(string subjectId);
+
+        public Task<IEnumerable<Report>> GetByFieldesAsync(string theater,string sector,string location);
+
+        public Task<IEnumerable<Report>> GetByPriorityAndDateAsync(List<string>? priorities, DateTime? fromThis, DateTime? toThis);
+
+       
+        public Task<IEnumerable<Report>> GetByParametersAsync(string? text , string? theater, string? sector, string? location,string[]? priorities, DateTime? fromThis, DateTime? toThis);
+
+        public Task<StatisticsResponse> GstStatisticsAsync();
+    }
+}
