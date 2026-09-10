2af17345e5363c2de3f87cedb88e31aa52c23b4f
avraham <aviv4134@gmail.com>
2026-09-10 17:17:37 +0300

    add globalization handeler

---
+++ CSharpServies/ReportingPlatform/ReportApi/ErrorHandeler/ReportsMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace ReportApi.middleware
+{
+    public class CustomExceptionHandler : IExceptionHandler
+    {
+        public CustomExceptionHandler()
+        {
+        }
+        public async ValueTask<bool> TryHandleAsync(
+            HttpContext httpContext,
+            Exception exception,
+            CancellationToken cancellationToken)
+        {
+            
+            if (exception is InvalidOperationException elasticErorr)
+            {
+
+                httpContext.Response.StatusCode = 500;
+
+                await httpContext.Response.WriteAsJsonAsync(new
+                {
+                    //error = "the elastic search server is offline"
+                    error = exception.Message
+                },cancellationToken);
+                
+                return true;
+
+            }
+
+            httpContext.Response.StatusCode = 500;
+
+            await httpContext.Response.WriteAsJsonAsync(new
+            {
+                error = $"server error : {exception.Message}"
+            },cancellationToken);
+                        
+            return true;
+        }
+    }
+}
